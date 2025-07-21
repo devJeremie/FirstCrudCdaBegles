2ec88c3a2677bc43f82640a3f8c183c0e8d5902c
devJeremie <chabanaisjeremie@gmail.com>
2025-07-21 12:00:44 +0200

    FristCrudBeglesCda

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Crud;
+use App\Form\CrudType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomePageController extends AbstractController
 {
@@ -13,6 +16,18 @@ final class HomePageController extends AbstractController
     {
         return $this->render('home_page/homePage.html.twig', [
             'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    #[Route('/create', name: 'app_create_form')]
+    public function create_form(Request $request): Response
+    {
+        $crud = new Crud();
+        $form = $this->createForm(CrudType::class, $crud);
+        $form->handleRequest($request);
+
+        return $this->render('form/createForm.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
